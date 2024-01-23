ebe9f675baeae50cb8a79f43bd40dae9c64f29f4
Happy Singh <118984911+HappySingh1901@users.noreply.github.com>
2024-01-23 14:51:07 +0530

    Add files via upload

---
+++ datatypes.R
@@ -0,0 +1,24 @@
+my_var1 <- 30            #numeric type
+print(class(my_var1))
+
+my_var2 <- "Hello world" #character type(string)
+print(class(my_var2))
+
+my_var3 <- 35L           #integer
+print(class(my_var3))
+
+my_var4 <- 8i+4          #complex number
+print(class(my_var4))
+
+my_var5 <- TRUE          #Logical/boolean 
+print(class(my_var5))
+
+my_var6 <- FALSE
+print(class(my_var6))
+
+my_var7 <- 99.5         #float or decimal numbers are also considered as numeric 
+print(class(my_var7))    
+
+
+a=5i
+print(class(a))
