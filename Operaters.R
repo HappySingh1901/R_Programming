8dafee0003fa75f04496270ef993d8888348ab72
Happy Singh <118984911+HappySingh1901@users.noreply.github.com>
2024-01-24 15:34:06 +0530

    Add files via upload

---
+++ Operaters.R
@@ -0,0 +1,43 @@
+# vector sum
+
+vector1 <- c(2,4,5.5,8)       #c here is to combine values into a vector  
+vector2 <- c(2,4,7,1)
+print(vector1+vector2)
+
+# Vector difference 
+
+vector3 <- c(2,5.9,6)
+vector4 <- c(8, 3, 4.7)
+print(vector3-vector4)
+
+# Vector multiplication
+
+vector5 <- c(2,4,8.5)
+vector6 <- c(6,9.7,2)
+print(vector5*vector6)
+
+
+# Vector division
+
+vector7 <- c(22,49,89.5)
+vector8 <- c(64,9.7,25)
+print(vector7/vector8)
+
+# Vector remainder 
+
+vector9 <- c(2,4,8.5)
+vector10 <- c(6,9.7,2)
+print(vector9%%vector10)
+
+# Vector quotient
+
+vector11 <- c(3,4,8.5)
+vector12 <- c(2,9.7,2)
+print(vector11%/%vector12)
+
+
+# Vector Exponent
+
+vector13 <- c(2,4,8.5)
+vector14 <- c(6,9.7,2)
+print(vector13^vector14)
