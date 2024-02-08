aa690baaf65548a85af0def9b99f6fea0459b471
Happy Singh <118984911+HappySingh1901@users.noreply.github.com>
2024-02-09 02:21:12 +0530

    Add files via upload

---
+++ math.R
@@ -0,0 +1,25 @@
+#Math functions in R
+
+max(29,57,72,84,91,10001,2045,2,98)
+min(5874,6356,7294,8348,73,82,894483,37)
+sqrt(34715664)
+exp(25)
+abs(-438238.7342)
+ceiling(1372928.4)
+ceiling(-132382.4)
+floor(132894.4)
+floor(-13732892.4)
+#User input 
+sum(a=as.numeric(readline()),b=as.numeric(readline()),c=as.numeric(readline()))
+prod(a=as.numeric(readline()),b=as.numeric(readline()),c=as.numeric(readline()))
+trunc(1.4)
+round (2.149,2) #Answer 2.15
+
+#trigonometric functions
+
+cos(1)
+sin(1)
+tan(1)
+log(8)
+log2(8)
+log10(8)
