70c1254cd380baf065bdf1874557e449e2b10502
Happy Singh <118984911+HappySingh1901@users.noreply.github.com>
2024-04-24 08:29:52 +0530

    Add files via upload

---
+++ Linegraphs.R
@@ -0,0 +1,12 @@
+##Line graphs
+
+a<-c(4,6,2,7,5)
+b<-c(5,6,2,7,8,9)
+plot(a,type = "o")
+plot(a,type = "s")
+plot(a,type = "o",col="red")
+
+##lines function is used to compare two lines
+lines(a,type="o")
+lines(b,type="o")
+                          
